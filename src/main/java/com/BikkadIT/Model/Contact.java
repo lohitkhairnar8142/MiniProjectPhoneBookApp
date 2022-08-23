36c62606a25bac1308744d5aadee433d6af085f8
lohitkhairnar8142 <lohitkhairnar11721@gmail.com>
2022-08-22 21:39:48 -0700

    Added Contacts Fields

---
+++ src/main/java/com/BikkadIT/Model/Contact.java
@@ -0,0 +1,9 @@
+package com.BikkadIT.Model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Contact {
+ 
+	
+}
---
+++ src/main/java/com/BikkadIT/Repository/ContactRepo.java
@@ -0,0 +1,7 @@
+package com.BikkadIT.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepo {
+
+}
